ff0f06df5e732a6b2d4e7ec897656962e266b184
selectthegang <66577268+selectthegang@users.noreply.github.com>
2021-10-15 03:04:43 +0000

    added context menu to home and report page

--- Home/script.js
+++ Home/script.js
@@ -4,6 +4,20 @@ let search = document.getElementById('search');
 let number = document.getElementById('number');
 let siteresults = document.getElementById('results');
 let menu = document.getElementById("mobile-menu");
+let contextMenu = document.getElementById("contextMenu");
+
+function getQuery(name, url = window.location.href) {
+  name = name.replace(/[\[\]]/g, '\\$&');
+  var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+    results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
+if (getQuery('number')) {
+  number.value = getQuery('number');
+}
 
 /* Load Service Worker (PWA)
 if ('serviceWorker' in navigator) {
@@ -19,28 +33,75 @@ if ('serviceWorker' in navigator) {
 	});
 }*/
 
+/* Search Number onEnter */
+number.addEventListener("keyup", function (event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    search.click();
+  }
+});
+
 /* Search Number When "Search" Button Is Clicked */
 search.addEventListener('click', function () {
-	if (number.value.length === 10) {
-		socket.emit('getNumber', number.value);
-	} else {
-		siteresults.innerHTML = `<div class="text-white font-sans font-bold">you didn't specify a vaild phone number</div>`;
-	}
+  if (number.value.length === 10) {
+    socket.emit('getNumber', number.value);
+  } else {
+    siteresults.innerHTML = `<div class="text-white font-sans font-bold">you didn't specify a vaild phone number</div>`;
+  }
 });
 
 /* Result Socket */
 socket.on('results', results => {
-	siteresults.innerHTML = `<div class="text-white font-sans font-bold">${results}</div>`;
+  siteresults.innerHTML = `<div class="text-white font-sans font-bold">${results}</div>`;
 });
 
 /* Open Menu Function */
 function openMenu() {
   let currentClass = menu.className;
 
-  if (currentClass === "hidden"){
+  if (currentClass === "hidden") {
     menu.className = "";
   }
   else {
     menu.className = "hidden";
+  }
+}
+
+// Context Menu
+window.addEventListener("contextmenu", function (event) {
+  event.preventDefault();
+  contextMenu.style.top = event.offsetY + "px";
+  contextMenu.style.left = event.offsetX + "px";
+  contextMenu.classList.add("active");
+});
+window.addEventListener("click", function () {
+  contextMenu.classList.remove("active");
+});
+
+// Context Menu Options
+function copy() {
+  number.select();
+  document.execCommand("copy");
+  siteresults.innerHTML = `<div class="text-white font-sans font-bold">Copied to Clipboard!</div>`;
+}
+function paste() {
+  navigator.clipboard.readText().then(text => {
+    number.value = text;
+  }).catch(() => {
+    siteresults.innerHTML = `<div class="text-white font-bold">Sorry, i am unable to paste the last thing you copied! Please ensure that you given us permission to "See text and images copied to the clipboard" by setting the "Clipboard" permission to Allowed on your browser site settings!</div>`;
+  });
+}
+function report() {
+  let currNumber = number.value;
+
+  if (currNumber === "") {
+    if (number.value.length === 10) {
+      socket.emit('getNumber', number.value);
+    } else {
+      siteresults.innerHTML = `<div class="text-white font-sans font-bold">you didn't specify a vaild phone number</div>`;
+    }
+  }
+  else {
+    window.location.href = `/report?number=${currNumber}`;
   }
 }
--- Report/script.js
+++ Report/script.js
@@ -4,32 +4,87 @@ let category = document.getElementById('type');
 let btn = document.getElementById('btn');
 let msg = document.getElementById('msg');
 let menu = document.getElementById("mobile-menu");
+let contextMenu = document.getElementById("contextMenu");
 
-btn.addEventListener('click', function() {
-	if (category.options[category.selectedIndex].text === 'Scam Category') {
-		msg.innerHTML = `<div class="text-white font-sans font-bold">you didn't specify the scam category</div>`;
-	} else {
-		if (number.value.length === 10) {
-			socket.emit(
-				'report',
-				number.value,
-				category.options[category.selectedIndex].text
-			);
-			msg.innerHTML = `<div class="text-white font-sans font-bold">thanks for reporting!</div>`;
-		} else {
-			msg.innerHTML = `<div class="text-white font-sans font-bold">you didn't specify a vaild phone number</div>`;
-		}
-	}
+function getQuery(name, url = window.location.href) {
+  name = name.replace(/[\[\]]/g, '\\$&');
+  var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+    results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
+if (getQuery('number')) {
+  number.value = getQuery('number');
+}
+
+btn.addEventListener('click', function () {
+  if (category.options[category.selectedIndex].text === 'Scam Category') {
+    msg.innerHTML = `<div class="text-white font-sans font-bold">you didn't specify the scam category</div>`;
+  } else {
+    if (number.value.length === 10) {
+      socket.emit(
+        'report',
+        number.value,
+        category.options[category.selectedIndex].text
+      );
+      msg.innerHTML = `<div class="text-white font-sans font-bold">thanks for reporting!</div>`;
+    } else {
+      msg.innerHTML = `<div class="text-white font-sans font-bold">you didn't specify a vaild phone number</div>`;
+    }
+  }
 });
 
 /* Open Menu Function */
 function openMenu() {
   let currentClass = menu.className;
 
-  if (currentClass === "hidden"){
+  if (currentClass === "hidden") {
     menu.className = "";
   }
   else {
     menu.className = "hidden";
+  }
+}
+
+// Context Menu
+window.addEventListener("contextmenu", function (event) {
+  event.preventDefault();
+  contextMenu.style.top = event.offsetY + "px";
+  contextMenu.style.left = event.offsetX + "px";
+  contextMenu.classList.add("active");
+
+  console.log(event.target);
+});
+window.addEventListener("click", function () {
+  contextMenu.classList.remove("active");
+});
+
+// Context Menu Options
+function copy() {
+  number.select();
+  document.execCommand("copy");
+  msg.innerHTML = `<div class="text-white font-sans font-bold">Copied to Clipboard!</div>`;
+}
+function paste() {
+  navigator.clipboard.readText().then(text => {
+    number.value = text;
+  }).catch(() => {
+    msg.innerHTML = `<div class="text-white font-bold">Sorry, i am unable to paste the last thing you copied! Please ensure that you given us permission to "See text and images copied to the clipboard" by setting the "Clipboard" permission to Allowed on your browser site settings!</div>`;
+  });
+}
+function check() {
+  let currNumber = number.value;
+
+  if (currNumber === "") {
+    if (number.value.length === 10) {
+      socket.emit('getNumber', number.value);
+    } else {
+      siteresults.innerHTML = `<div class="text-white font-sans font-bold">you didn't specify a vaild phone number</div>`;
+    }
+  }
+  else {
+    window.location.href = `/?number=${currNumber}`;
   }
 }
